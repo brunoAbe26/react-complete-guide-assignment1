ca86414e53cacd6f0f95a8f46f661e1c584c4866
bruno.abe <bh_abe1@hotmail.com>
2018-11-12 12:19:26 -0200

    first commit

---
+++ assignment1/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
+
+class App extends Component {
+	state = {
+		username: 'Koichi'
+	};
+
+	usernameChangedHandler = (event) => {
+		this.setState({username: event.target.value});
+	}
+
+  render() {
+    return (
+			<div className='App'>
+				<UserInput changed={this.usernameChangedHandler} currentName={this.state.username}/>
+				<UserOutput userName={this.state.username}/>
+				<UserOutput userName={this.state.username}/>
+				<UserOutput userName='Max'/>
+			</div>
+		);
+	}
+}
+
+export default App;
